437b645e4c8327db6c27b197f984595045307a38
NickolayMetapic <nikolay@metapic.com>
2023-09-22 13:59:55 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>Categories:</div>
+    <p v-if="!categories">No categories</p>
+    <ul v-else>
+        <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <button @click="getCategories">Get categories</button>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const categories = ref<Array<string> | undefined>(undefined)
+
+const getCategories = async () => {
+    const response = await fetch("http://localhost/api/categories");
+    categories.value = await response.json();
+}
+</script>
+
+<style scoped>
+</style>
