87802e845619daefc3a1efa9c587e9632487b41d
Buink <armatasplay@gmail.com>
2023-09-24 20:13:40 +0300

    Add prettier file

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This is repository of frontend part of hollandseeds web-site
 
 ## Recommended IDE Setup
 
-- WebStorm
-- node v 18.17
+-   WebStorm
+-   node v 18.17
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
-  base: "/hollandseeds/",
-  plugins: [vue()],
-});
+    base: "/hollandseeds/",
+    plugins: [vue()],
+})
