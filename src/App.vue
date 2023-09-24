04d296187546a8710763f238e13752617ba9cb8c
Buink <armatasplay@gmail.com>
2023-09-24 20:21:50 +0300

    Prettier test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-    <h2>This is test site for auto deploys</h2>
-    <div>Categories:</div>
-    <p v-if="!categories">No categories</p>
-    <ul v-else>
-        <li v-for="cat in categories" :key="cat">{{ cat }}</li>
-    </ul>
-    <button @click="getCategories">Get categories</button>
+    <h2>This is test site for prettier auto deploys</h2><div>Categories:</div><p v-if="!categories">No categories</p><ul v-else><li v-for="cat in categories" :key="cat">{{ cat }}</li></ul><button @click="getCategories">Get categories</button>
 </template>
 
 <script setup lang="ts">
